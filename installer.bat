13fdfda209b8ec06c9684417385fb64ab1cd8a0e
mahdichantoufiLOQ <ch.mahdi.96@gmail.com>
2026-05-15 19:52:31 +0200

    installer.bat: create desktop shortcut after install

--- installer.bat
+++ installer.bat
@@ -1,5 +1,9 @@
 @echo off
 pip install pyperclip
+
+echo Creation du raccourci sur le bureau...
+powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut([Environment]::GetFolderPath('Desktop') + '\WriteHelper.lnk'); $s.TargetPath = 'pythonw.exe'; $s.Arguments = '\"%~dp0gui.pyw\"'; $s.WorkingDirectory = '%~dp0'; $s.IconLocation = 'pythonw.exe,0'; $s.Save()"
+
 echo.
-echo Installation terminee !
+echo Installation terminee ! Raccourci cree sur le bureau.
 pause
